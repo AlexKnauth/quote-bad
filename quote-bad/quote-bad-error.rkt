b51fa6005d5ff08746c76ef7c76ef5ef206c5284
AlexKnauth <alexander@knauth.org>
2016-06-06 12:02:34 -0400

    move raise-quote-bad-error functions to a separate file

---
+++ quote-bad/quote-bad-error.rkt
@@ -0,0 +1,49 @@
+#lang racket/base
+
+(provide raise-quote-bad-error
+         raise-self-quote-bad-error
+         )
+
+(require racket/port
+         racket/pretty
+         "translate-quoted.rkt"
+         )
+
+;; raise-quote-bad-error : Syntax Syntax -> Nothing
+;; Raises a syntax error explaining what to use instead of quote.
+(define (raise-quote-bad-error stx stuff)
+  ;; translated-s-expr : S-Expr
+  (define translated-s-expr
+    (translate-quoted-s-expr stuff))
+  ;; translated-pretty : String
+  (define translated-pretty
+    (pretty-write-string translated-s-expr))
+  ;; message : String
+  (define message
+    (format "Don't use quote for this. Instead you can use\n~a" translated-pretty))
+  (raise-syntax-error #f message stx))
+
+;; raise-self-quote-bad-error : Syntax Syntax -> Nothing
+;; Raises a syntax error explaining what to use instead of a self-quoting literal.
+(define (raise-self-quote-bad-error stx stuff)
+  ;; translated-s-expr : S-Expr
+  (define translated-s-expr
+    (translate-quoted-s-expr stuff))
+  ;; translated-pretty : String
+  (define translated-pretty
+    (pretty-write-string translated-s-expr))
+  ;; message : String
+  (define message
+    (format (string-append
+             "Don't use self-quoting compound literals that implicitly quote sub-expressions.\n"
+             "Instead you can use\n"
+             "~a")
+            translated-pretty))
+  (raise-syntax-error #f message stx))
+
+;; pretty-write-string : S-Expr -> String
+(define (pretty-write-string s-expr)
+  (call-with-output-string
+   (λ (out)
+     (pretty-write s-expr out))))
+
--- quote-bad/quote-bad.rkt
+++ quote-bad/quote-bad.rkt
@@ -4,56 +4,15 @@
 
 (require (only-in racket/base [quote rkt:quote] [#%datum rkt:#%datum])
          (for-syntax racket/base
-                     racket/port
-                     racket/pretty
                      syntax/parse
                      "translate-quoted.rkt"
+                     "quote-bad-error.rkt"
                      ))
 (module+ test
   (require racket/match
            rackunit
            (only-in unstable/macro-testing convert-compile-time-error)
            ))
-
-(begin-for-syntax
-  ;; raise-quote-bad-error : Syntax Syntax -> Nothing
-  ;; Raises a syntax error explaining what to use instead of quote.
-  (define (raise-quote-bad-error stx stuff)
-    ;; translated-s-expr : S-Expr
-    (define translated-s-expr
-      (translate-quoted-s-expr stuff))
-    ;; translated-pretty : String
-    (define translated-pretty
-      (pretty-write-string translated-s-expr))
-    ;; message : String
-    (define message
-      (format "Don't use quote for this. Instead you can use\n~a" translated-pretty))
-    (raise-syntax-error #f message stx))
-
-  ;; raise-self-quote-bad-error : Syntax Syntax -> Nothing
-  ;; Raises a syntax error explaining what to use instead of a self-quoting literal.
-  (define (raise-self-quote-bad-error stx stuff)
-    ;; translated-s-expr : S-Expr
-    (define translated-s-expr
-      (translate-quoted-s-expr stuff))
-    ;; translated-pretty : String
-    (define translated-pretty
-      (pretty-write-string translated-s-expr))
-    ;; message : String
-    (define message
-      (format (string-append
-               "Don't use self-quoting compound literals that implicitly quote sub-expressions.\n"
-               "Instead you can use\n"
-               "~a")
-              translated-pretty))
-    (raise-syntax-error #f message stx))
-
-  ;; pretty-write-string : S-Expr -> String
-  (define (pretty-write-string s-expr)
-    (call-with-output-string
-     (λ (out)
-       (pretty-write s-expr out))))
-  )
 
 (define-syntax quote
   (lambda (stx)
