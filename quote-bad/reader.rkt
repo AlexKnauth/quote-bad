8da5d8bca6d8fb0c07d9ab71ee3d0957fda50b48
AlexKnauth <alexander@knauth.org>
2016-06-06 14:04:06 -0400

    error at read-time instead of expansion-time for #lang quote-bad
    Still doesn’t handle self-quoting compound data such as vector literals.

--- quote-bad/reader.rkt
+++ quote-bad/reader.rkt
@@ -3,27 +3,30 @@
 (provide wrap-reader)
 
 (require hygienic-quote/private/make-quote-proc
-         (for-meta -10 (only-in quote-bad/quote-bad quote))
-         (for-meta -9 (only-in quote-bad/quote-bad quote))
-         (for-meta -8 (only-in quote-bad/quote-bad quote))
-         (for-meta -7 (only-in quote-bad/quote-bad quote))
-         (for-meta -6 (only-in quote-bad/quote-bad quote))
-         (for-meta -5 (only-in quote-bad/quote-bad quote))
-         (for-meta -4 (only-in quote-bad/quote-bad quote))
-         (for-meta -3 (only-in quote-bad/quote-bad quote))
-         (for-meta -2 (only-in quote-bad/quote-bad quote))
-         (for-meta -1 (only-in quote-bad/quote-bad quote))
-         (for-meta 0 (only-in quote-bad/quote-bad quote))
-         (for-meta 1 (only-in quote-bad/quote-bad quote))
-         (for-meta 2 (only-in quote-bad/quote-bad quote))
-         (for-meta 3 (only-in quote-bad/quote-bad quote))
-         (for-meta 4 (only-in quote-bad/quote-bad quote))
-         (for-meta 5 (only-in quote-bad/quote-bad quote))
-         (for-meta 6 (only-in quote-bad/quote-bad quote))
-         (for-meta 7 (only-in quote-bad/quote-bad quote))
-         (for-meta 8 (only-in quote-bad/quote-bad quote))
-         (for-meta 9 (only-in quote-bad/quote-bad quote))
-         (for-meta 10 (only-in quote-bad/quote-bad quote))
+         syntax/parse
+         "translate-quoted.rkt"
+         "quote-bad-error.rkt"
+         (for-meta -10 racket/base)
+         (for-meta -9 racket/base)
+         (for-meta -8 racket/base)
+         (for-meta -7 racket/base)
+         (for-meta -6 racket/base)
+         (for-meta -5 racket/base)
+         (for-meta -4 racket/base)
+         (for-meta -3 racket/base)
+         (for-meta -2 racket/base)
+         (for-meta -1 racket/base)
+         (for-meta 0 racket/base)
+         (for-meta 1 racket/base)
+         (for-meta 2 racket/base)
+         (for-meta 3 racket/base)
+         (for-meta 4 racket/base)
+         (for-meta 5 racket/base)
+         (for-meta 6 racket/base)
+         (for-meta 7 racket/base)
+         (for-meta 8 racket/base)
+         (for-meta 9 racket/base)
+         (for-meta 10 racket/base)
          )
 
 ;; wrap-reader : [A ... -> Any] -> [A ... -> Any]
@@ -33,6 +36,15 @@
 ;; make-quote-bad-readtable : Readtable [Syntax -> Syntax] -> Readtable
 (define (make-quote-bad-readtable orig-rt outer-scope)
   (make-readtable orig-rt
-    #\' 'terminating-macro (make-quote-proc #'quote outer-scope)
+    #\' 'terminating-macro (wrap-quote-proc (make-quote-proc #'quote outer-scope))
     ))
+
+;; wrap-quote-proc : Readtable-Proc -> Readtable-Proc
+(define ((wrap-quote-proc quote-proc) char in src ln col pos)
+  (define stx (quote-proc char in src ln col pos))
+  (syntax-parse stx
+    [(quote id:id) stx]
+    [(quote kw:keyword) stx]
+    [(quote simple) #:when (atomic-literal-data? (syntax-e #'simple)) stx]
+    [(quote stuff) (raise-quote-bad-error stx #'stuff)]))
 
