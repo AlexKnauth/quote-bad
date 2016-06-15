ed58fcd18c1566b8c6b7fee1ed75f2429f7b64f0
AlexKnauth <alexander@knauth.org>
2016-06-14 23:19:18 -0400

    refactor constructor-style-print/lang/runtime-config

--- constructor-style-print/lang/runtime-config.rkt
+++ constructor-style-print/lang/runtime-config.rkt
@@ -7,21 +7,27 @@
          )
 
 (define (configure data)
-  (define orig-port-print-handler (global-port-print-handler))
-  (define orig-printer (current-print))
+  (global-port-print-handler
+   (make-constructor-style-port-print-handler (global-port-print-handler)))
+  (current-print
+   (make-constructor-style-printer (current-print))))
+
+;; make-constructor-style-print-handler :
+;; (Any Output-Port [(U 0 1)] -> Void) -> (Any Output-Port [(U 0 1)] -> Void)
+(define (make-constructor-style-port-print-handler orig-port-print-handler)
   ;; constructor-style-port-print-handler : Any Output-Port [(U 0 1)] -> Void
-  ;; The new value for the global-port-print-handler.
   (define (constructor-style-port-print-handler v out [qdepth 0])
     (parameterize ([global-port-print-handler orig-port-print-handler])
       (constructor-style-print v out qdepth)))
+  constructor-style-port-print-handler)
+
+;; make-constructor-style-printer : (Any -> Void) -> (Any -> Void)
+(define (make-constructor-style-printer orig-printer)
   ;; constructor-style-printer : Any -> Void
   (define (constructor-style-printer v)
     (when (not (void? v))
       (orig-printer (written (print-convert/constructor-style v)))))
-  (global-port-print-handler constructor-style-port-print-handler)
-  (current-print constructor-style-printer))
-
-
+  constructor-style-printer)
 
 ;; A struct for wrapping values that are printed as an s-expression or
 ;; other value is written
